f7777f3a95794b0086e22199731bf5d3cadc68f4
Simon Erhardt <hello@rootlogin.ch>
2018-08-18 18:24:31 +0200

    renamed

--- events.go
+++ events.go
@@ -6,23 +6,23 @@ type EventTable struct {
 	events map[string][]func(...interface{})
 }
 
-var instance *Events
+var instance *EventTable
 var once sync.Once
 
-func Events() *Events {
+func Events() *EventTable {
 	once.Do(func() {
-		instance = &Events{
+		instance = &EventTable{
 			events: make(map[string][]func(...interface{})),
 		}
 	})
 	return instance
 }
 
-func (e *Events) On(event string, do func(...interface{})) {
+func (e *EventTable) On(event string, do func(...interface{})) {
 	e.events[event] = append(e.events[event], do)
 }
 
-func (e *Events) Emit(event string, args ...interface{}) {
+func (e *EventTable) Emit(event string, args ...interface{}) {
 	for _, f := range e.events[event] {
 		f(args...)
 	}
